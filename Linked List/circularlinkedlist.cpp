dfa99b114dba278cef4eec28561e4ecc00c147bc
ameybikram <a.bikram492@gmail.com>
2019-06-27 02:17:42 +0530

    Update circularlinkedlist.cpp

--- Linked List/circularlinkedlist.cpp
+++ Linked List/circularlinkedlist.cpp
@@ -20,8 +20,11 @@ public:
 	}
 	void insertatbegin(int);
 	void insertatlast(int);
+	void insertatanypos(int, int);
 	int deletefirstnode();
 	int deletelastnode();
+	int deleteatanypos(int);
+	int length();
 	void display();
 };
 
@@ -62,6 +65,34 @@ void circularll::insertatlast(int data)
 	}
 }
 
+void circularll::insertatanypos(int x, int pos)
+
+{
+	node *temp,*ptr;
+	ptr= head;
+	if(head == NULL && pos == 0)
+	{
+		temp = new node;
+		temp->data = x;
+		head= temp;
+		temp->next = head;
+		tail = temp;
+	}
+	else if(head == NULL && pos>0)
+		return ;
+	else
+	{
+		for(int i=1;i<=pos-1 && ptr;i++)
+			ptr = ptr->next;
+		if(ptr->next == head)
+			insertatlast(x);
+		else if(ptr)
+		{
+			temp->next = ptr->next;
+			ptr->next = temp;
+		}
+	}
+}
 int circularll::deletefirstnode()
 {
 	if(head==NULL)
@@ -104,6 +135,55 @@ int circularll::deletelastnode()
 		return del_data;
 	}
 }
+
+int circularll::deleteatanypos(int pos)
+{
+	int del_data;
+	node *q;
+	if(head == NULL)
+	{
+		cout<<"No list found"<<endl;
+		return 0;
+	}
+	if(pos<0 || pos>length())
+		return -1;
+	if(pos == 0)
+		deletefirstnode();
+	else
+	{
+		node *ptr = head;
+		for (int i = 1; i <= pos - 2; ++i)
+			ptr = ptr->next;
+		if(ptr->next->next == head)
+			deletelastnode();
+		else
+		{
+			del_data = ptr->next->data;
+			q = ptr->next;
+			del_data = q->data;
+			free(q);
+			ptr->next = ptr->next->next;
+			return del_data;
+		}
+	}
+}
+int circularll::length()
+{
+	node *ptr = head;
+	int count = 0;
+	if(head == NULL)
+		return 0;
+	else
+	{
+		do
+		{
+			ptr = ptr->next;
+			count++;
+		}while(ptr!=head);
+		return count;
+	}
+	
+}
 void circularll::display()
 {
 	node *current;
@@ -128,7 +208,7 @@ int main()
 	while(1)
 	{
 		cout<<"\n**** MENU ****"<<endl;
-        cout<<"1:INSERT_AT_BEGIN\n2:INDERT_AT_LAST\n3:DELETE_FIRST_NODE\n4:DELETE_LASTNODE\n5:DISPLAY\n6:EXIT\n";
+        cout<<"1:INSERT_AT_BEGIN\n2:INDERT_AT_LAST\n3:DELETE_FIRST_NODE\n4:DELETE_LASTNODE\n5:DISPLAY\n6:INSERT_AT_ANY_POS\n7:DELETE_AT_ANY_POS\n8:EXIT\n";
         cout<<"\nEnter Your Choice:";
         cin>>ch;
         switch(ch)
@@ -145,7 +225,15 @@ int main()
         			break;
         	case 5: l.display();
         			break;
-        	case 6: return 0;
+        	case 6: cin>>data>>pos;
+        			l.insertatanypos(data,pos);
+        			break;
+
+        	case 7: cin>>pos;
+        			l.deleteatanypos(pos);
+        			break;
+
+        	case 8: return 0;
 
         }
 	}
