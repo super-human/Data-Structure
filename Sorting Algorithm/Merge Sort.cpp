c75857094e85d514eda2c4bbfafa29fc29a9ccb2
ameybikram <a.bikram492@gmail.com>
2019-09-28 07:28:34 +0530

    Create Merge Sort.cpp

---
+++ Sorting Algorithm/Merge Sort.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+void merge(int arr[], int l, int mid, int h)
+{
+    int i,j,k;
+    i=l,j=mid+1,k = l;
+    int B[h+1];
+    while(i<=mid && j <= h)
+    {
+        if(arr[i] < arr[j])
+        {
+            B[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    for(;i<=mid; i++)
+    {
+        B[k] = arr[i];
+        k++;
+    }
+    for(;j<=h; j++)
+    {
+        B[k] = arr[j];
+        k++;
+    }
+    for(i = l; i<=h; i++)
+        arr[i] = B[i];
+}
+
+void mergesort(int arr[], int l, int h)
+{
+    if(l<h)
+    {
+        int mid = (l+h)/2;
+        mergesort(arr,l,mid);
+        mergesort(arr,mid+1,h);
+        merge(arr,l,mid,h);
+    }
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    std::cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++)
+        std::cin>>arr[i];
+    mergesort(arr,0,n-1);
+    for(int i = 0; i<n; i++)
+        std::cout<<arr[i]<<" ";
+    std::cout<<'\n';
+return 0;
+
+}
